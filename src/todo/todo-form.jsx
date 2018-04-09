355bed369b437187947c3fdae22ff8cb6b1e9212
Juliano <juliano@nationphone.com>
2018-04-09 00:05:26 -0300

    UPDATE - funcionalidades adicionar remover e listar

--- src/todo/todo-form.jsx
+++ src/todo/todo-form.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import IconButton from '../template/icon-button'
 
 export default props => (
-    <div>
-        <h2>Formuario</h2>
+    <div className='todoForm' role='form'>
+        <div className="col-xs-12 col-sm-9 col-md-10">
+            <input id="description" className="form-control" 
+            onChange={props.handleChange}
+            placeholder="Adicione uma tarefa" value={props.description}></input>
+        </div>
+        <div className="col-xs-12 col-sm-3 col-md-2">
+            <IconButton style='primary' icon='plus'
+                onClick={props.handleAdd}></IconButton>
+        </div>
     </div>
 )
--- src/todo/todo-list.jsx
+++ src/todo/todo-list.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import IconButton from '../template/icon-button'
+ 
+export default props => {
 
-export default props =>(
-    <div>
-        <h2>Lista</h2>
-    </div>
-)
+    const renderRows = () => {
+        const list = props.list || []
+        return list.map(todo => (
+            <tr  key={todo._id}>
+                <td>{todo.description}</td>
+                <td>
+                    <IconButton style='danger' icon='trash-o'
+                        onClick={()=>props.handleRemove(todo)}></IconButton>
+                </td>
+            </tr>
+        ))
+    }
+
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
